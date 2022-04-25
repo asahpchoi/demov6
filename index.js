75b727b60eea40e0c1617986af90659da3577665
asahpchoi <asa.choi@gmail.com>
2022-04-25 18:36:25 +0800

    2 files modified

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 import React, { Suspense } from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter as Router, Link, Route, Routes } from 'react-router-dom';
+import {
+  BrowserRouter as Router,
+  Link,
+  Route,
+  Routes,
+  Outlet,
+} from 'react-router-dom';
 
 import './style.css';
 
@@ -65,26 +71,60 @@ const Nav = () => {
   );
 };
 
+const Flight = () => {
+  return <>Flight</>;
+};
+
+const City = () => {
+  return <></>;
+};
+const Island = () => {
+  return <></>;
+};
+
 const App = () => (
   <>
-    <div id="container">
-      <header>
-        <h1 contenteditable>
-          <Banner />
-        </h1>
-      </header>
-      <div class="left-sidebar" contenteditable>
-        <Nav />
+    <Routes>
+      <Route path="/" element={<Flight />}>
+        <Route path="city" element={<City />} />
+        <Route path="island" element={<Island />} />
+      </Route>
+    </Routes>
+    <div class="header">
+      <h1>Chania</h1>
+    </div>
+
+    <div class="row">
+      <div class="col-3 col-s-3 menu">
+        <ul>
+          <li>The Flight</li>
+          <li>The City</li>
+          <li>The Island</li>
+          <li>The Food</li>
+        </ul>
       </div>
-      <main contenteditable>
-        <Suspense fallback={null}>
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="users" element={<User />} />
-          </Routes>
-        </Suspense>
-      </main>
-      <footer contenteditable>Footer Content — Header.com 2020</footer>
+
+      <div class="col-6 col-s-9">
+        <Outlet />
+      </div>
+
+      <div class="col-3 col-s-12">
+        <div class="aside">
+          <h2>What?</h2>
+          <p>Chania is a city on the island of Crete.</p>
+          <h2>Where?</h2>
+          <p>Crete is a Greek island in the Mediterranean Sea.</p>
+          <h2>How?</h2>
+          <p>You can reach Chania airport from all over Europe.</p>
+        </div>
+      </div>
+    </div>
+
+    <div class="footer">
+      <p>
+        Resize the browser window to see how the content respond to the
+        resizing.
+      </p>
     </div>
   </>
 );
