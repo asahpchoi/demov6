8856b548bb77868db6525bf5532b88f881aefd7f
asahpchoi <asa.choi@gmail.com>
2022-04-26 22:34:22 +0800

    3 files modified

--- index.js
+++ index.js
@@ -1,8 +1,21 @@
 import { createRoot } from 'react-dom/client';
 import React from 'react';
+import Button from '@mui/material/Button';
 
 const App = () => {
-  return <div>Testing</div>;
+  return (
+    <div>
+      <Header />
+    </div>
+  );
+};
+
+const Header = () => {
+  return (
+    <div>
+      <Button variant="contained">Hello World</Button>
+    </div>
+  );
 };
 
 const container = document.getElementById('app');
